3dd6c40b014d7a854205ef038db64422203c0116
Alexander Fischer <tbanus1@gmail.com>
2013-11-17 16:13:50 +0100

    sprachdatei zugefuegt

---
+++ protected/messages/de/main.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Dies ist die deutsche Sprachdatei von DSAo-Md
+ * In den Sourcen werden lediglich englische Ausdrucke verwendet wie z.B. contact_us oder contact_us_intro
+ */
+return array(
+
+   /**
+    * Nachfolgend die allgemeinen Dinge, die entweder immer wieder auftauchen
+    * oder aber nirgends wo direkt zugeordnet werden koennen
+    */
+   'dsao-md'          => 'Das schwarze Auge online - Morgendämmerung',
+   'intro'            => 'Die lange Reise ist endlich zu Ende, Ihr seid am Ziel angekommen! Oder... fängt sie jetzt erst an?',
+   'contact_us'       => 'Kontaktiere uns',
+   'required'         => 'Felder die mit einem <span class="required">*</span> gekennzeichnet sind, werden benötigt',
+   'contact_us_intro' => 'Wenn Du mit uns in Kontakt treten willst, dann kannst du entweder die nachfolgenden Möglichkeiten nutzen, oder du verwendest das Kontaktformular.',
+   'captcha'          => 'Bitte gebe die Buchstaben aus dem Bild ein. Gross- und Kleinschreibung wird nicht beachtet',
+   'send'             => 'Absenden',
+   'contact_form'     => 'Kontaktformular',
+   'greetings'        => '"Seyed gegrüßt, Reisender!"',
+   'rp_editorial'     => 'Begrüßt Euch der Gardist zu Euerer linken Seite, als Ihr das Stadttor erreicht. Die warem morgendlichen Sonnenstrahlen fallen auf das doppelflüglige hölzerne Tor vor Euch. Ihr richtet Eueren Blick auf den Euch ansprechenden Mann. Ein roter Fuchs auf goldenem Grund prankt auf der Brust des mitte zwanzig Jährigen, das Ihr gerade noch erkennen könnt, ehe er sich verneigt und den linken Arm einladend gen Tor hebt. "Einen angenehmen Aufenthalt" ruft er Euch noch höflich hinterher, als ihr die Torschwelle überschreitet und die ersten Fassaden der stattlichen Bauten in Euer Blickfeld fallen...',
+	'ooc_editorial'    => 'So, oder so ähnlich kann sich die Einleitung zu einem Abenteuer abspielen, welches Du hier bald erleben kannst. Noch ist es leider nicht so weit, da wir mitten in der Entwicklung stecken (näheres unter dem Menüpunkt "Über uns") - aber täglich kommen neue Möglichkeiten und Fortschritte hinzu. Den aktuellen Fortschritt siehst du entweder in der Changelog als chronologische Zusamenfassung, in der Kategorie "Features" (ebenso im Menü-Punkt "Über uns") oder aber bei den Neuigkeiten, bzw. im Neuigkeiten-Archiv. Das DSAo-Md Team wünscht viel Spass und allzeit eine geschliffene Klinge!'
+        
+   /**
+    * Menu-Eintraege
+    */
+   'menu.home'        => 'Start',
+	'menu.about_us'    => 'Über uns',
+	'menu.project'     => 'Projekt',
+	'menu.history'     => 'Geschichte',
+	'menu.team'        => 'Das Team',
+	'menu.thanks'      => 'Danksagung',
+	'menu.contact'     => 'Kontaktiere uns',
+	'menu.login'       => 'Anmelden',
+	'menu.logout'      => 'Abmelden',
+	
+	/**
+	 * Seiten-Titel
+	 */
+	'title.about_us'   => 'Über uns',
+);
+?>
+
