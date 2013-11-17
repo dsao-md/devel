8afcd92f959cfa7fe83cc686de135b8a184b3725
Alexander Fischer <tbanus1@gmail.com>
2013-11-17 13:53:29 +0100

    Kommentare korrigiert und Einrückung bereinigt

--- protected/models/ContactForm.php
+++ protected/models/ContactForm.php
@@ -19,28 +19,36 @@ class ContactForm extends CFormModel
 	public function rules()
 	{
 		return array(
-			// Name, E-Mail, Betreff und Textkoerper muessen vorhanden sein
+         /**
+          * Name, E-Mail, Betreff und Textkoerper muessen vorhanden sein
+          */
 			array('name, email, subject, body', 'required'),
-			// Email-Adresse muss eine gueltige Emailadresse sein
+			
+			/**
+			 * Email-Adresse muss eine gueltige Emailadresse sein
+			 */
 			array('email', 'email'),
-			// Der Verifizierungscode muss korrekt eingegeben werden
+			
+			/**
+			 * Der Verifizierungscode muss korrekt eingegeben werden
+			 */
 			array('verifyCode', 'captcha', 'allowEmpty'=>!CCaptcha::checkRequirements()),
 		);
 	}
 
 	/**
-	 * Deklarierung der Formular-Beschriftungen.
+	 * Deklarieren der Formular-Beschriftungen.
 	 * Wenn nicht explizit Deklariert, wird das Attribut zu einem Label, 
 	 * Wobei der erste Buchstabe Gross geschrieben wird (besser wie nichts :-P)
 	 */
 	public function attributeLabels()
 	{
 		return array(
-			'verifyCode'=>'Verifizierungs-Code',
-			'name'=>'Vor- und Zuname',
-			'email'=>'E-Mail',
-			'subject'=>'Betreff',
-			'body'=>'Deine Nachricht',
+			'verifyCode' => 'Verifizierungs-Code',
+			'name'       => 'Vor- und Zuname',
+			'email'      => 'E-Mail',
+			'subject'    => 'Betreff',
+			'body'       => 'Deine Nachricht',
 		);
 	}
 }
